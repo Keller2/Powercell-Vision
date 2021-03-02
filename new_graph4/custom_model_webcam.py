c6f7f518c2d0424d747ce74f1b089fae1e4977a4
Keller2 <46767006+Keller2@users.noreply.github.com>
2021-03-01 20:48:08 -0600

    Add files via upload

---
+++ new_graph4/custom_model_webcam.py
@@ -0,0 +1,166 @@
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+import zipfile
+import time
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+from PIL import Image
+
+from object_detection.utils import label_map_util
+
+from object_detection.utils import visualization_utils as vis_util
+
+
+#from protos import string_int_label_map_pb2
+
+#from utils import label_map_util
+#from utils import visualization_utils as vis_util
+
+import cv2
+
+cap = cv2.VideoCapture(0)   # if you have multiple webcams change the value to the correct one
+
+
+# Any model exported using the `export_inference_graph.py` tool can be loaded here simply by changing `PATH_TO_CKPT` to point to a new .pb file.
+#
+# By default we use an "SSD with Mobilenet" model here. See the [detection model zoo](https://github.com/tensorflow/models/blob/master/object_detection/g3doc/detection_model_zoo.md) for a list of other models that can be run out-of-the-box with varying speeds and accuracies.
+
+# In[4]:
+
+# What model to download.
+MODEL_NAME = 'new_graph4'  # change to whatever folder has the new graph
+# MODEL_FILE = MODEL_NAME + '.tar.gz'   # these lines not needed as we are using our own model
+# DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+
+# List of the strings that is used to add correct label for each box.
+PATH_TO_LABELS = 'data/object-detection.pbtxt'
+
+NUM_CLASSES = 1  # we are only using one class in this example (cloth_mask)
+
+# we don't need to download model since we have our own
+# ## Download Model
+
+# In[5]: 
+#
+# opener = urllib.request.URLopener()
+# opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+# tar_file = tarfile.open(MODEL_FILE)
+# for file in tar_file.getmembers():
+#     file_name = os.path.basename(file.name)
+#     if 'frozen_inference_graph.pb' in file_name:
+#         tar_file.extract(file, os.getcwd())
+
+
+# ## Load a (frozen) Tensorflow model into memory.
+
+# In[6]:
+
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+
+# ## Loading label map
+# Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
+
+# In[7]:
+
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+
+
+
+def load_image_into_numpy_array(image):
+    (im_width, im_height) = image.size
+    return np.array(image.getdata()).reshape(
+        (im_height, im_width, 3)).astype(np.uint8)
+
+
+# # Detection
+
+# In[9]:
+
+# For the sake of simplicity we will use only 2 images:
+# image1.jpg
+# image2.jpg
+# If you want to test the code with your images, just add path to the images to the TEST_IMAGE_PATHS.
+PATH_TO_TEST_IMAGES_DIR = 'outputs2'
+TEST_IMAGE_PATHS = [os.path.join(PATH_TO_TEST_IMAGES_DIR, 'detection_output{}.png'.format(i)) for i in range(0, 100)]  # adjust range for # of images in folder
+PATH_TO_SHOW_IMAGE_DIR = 'output'
+# Size, in inches, of the output images.
+IMAGE_SIZE = (12, 8)
+
+
+# In[10]:
+
+with detection_graph.as_default():
+    with tf.Session(graph=detection_graph) as sess:
+        i = 0
+        for image_path in TEST_IMAGE_PATHS:
+            ret, preimg = cap.read()
+            
+            cv2.imwrite("outputs2/detection_output{}.png".format(i), preimg)
+            print("ok-")
+              # create an outputs folder for the images to be saved
+            time.sleep(0.1)
+            image = Image.open(image_path)
+            image_np = load_image_into_numpy_array(image)
+            # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+            #image_np = load_image_into_numpy_array(image)
+            image_np_expanded = np.expand_dims(image_np, axis=0)
+            image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+            # Each box represents a part of the image where a particular object was detected.
+            boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+            print (image_tensor)
+            # Each score represent how level of confidence for each of the objects.
+            # Score is shown on the result image, together with the class label.
+            scores = detection_graph.get_tensor_by_name('detection_scores:0')
+            classes = detection_graph.get_tensor_by_name('detection_classes:0')
+            num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+            
+            # Actual detection.
+            (boxes, scores, classes, num_detections) = sess.run(
+                [boxes, scores, classes, num_detections],
+                feed_dict={image_tensor: image_np_expanded})
+            # Visualization of the results of a detection.
+            vis_util.visualize_boxes_and_labels_on_image_array(
+                image_np,
+                np.squeeze(boxes),
+                np.squeeze(classes).astype(np.int64),
+                np.squeeze(scores),
+                category_index,
+                use_normalized_coordinates=True,
+                line_thickness=10)
+            
+            
+            plt.figure(figsize=IMAGE_SIZE)
+            plt.imshow(image_np)    # matplotlib is configured for command line only so we save the outputs instead
+            plt.savefig("outputs/detection_output{}.png".format(i)) 
+            print ("run ")
+            imgout = cv2.imread("outputs/detection_output{}.png".format(i))
+            
+            cv2.imshow("output", imgout)
+            i = i+1
+            #cv2.resize(IMAGE_SIZE)
+            #cv2.imshow(image_np)
+            cv2.waitKey(1)
+            #time.sleep(0.5)
+            #cv2.destroyAllWindows()
+            #if cv2.waitKey(0) & 0xFF == ord('q'):
+                #cv2.destroyAllWindows()
+               # break
